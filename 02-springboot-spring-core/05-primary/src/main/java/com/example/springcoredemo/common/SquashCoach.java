6a61c69af3785b564be7467b4c2c8dc8c7adb6a1
Joshua Chuah <xjoshua96@gmail.com>
2024-12-10 20:21:26 -0500

    primary annotation

---
+++ 02-springboot-spring-core/05-primary/src/main/java/com/example/springcoredemo/common/SquashCoach.java
@@ -0,0 +1,13 @@
+package com.example.springcoredemo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SquashCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout(){
+        return "Practice straight drives for 30 minutes";
+    }
+}
