1333eca6dc0a0ca52b4b53c0053b17f9a600de5a
Joshua Chuah <xjoshua96@gmail.com>
2024-12-12 17:58:26 -0500

    clean up code

--- 04-springboot-rest-crud/01-springboot-rest-crud/src/main/java/com/example/demo/rest/StudentRestController.java
+++ 04-springboot-rest-crud/01-springboot-rest-crud/src/main/java/com/example/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.example.demo.rest;
 
 import com.example.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +13,24 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoint for "/students" - return a list of students
+    private List<Student> students;
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+    // define @PostConstruct to load the student data ... only onece
+    @PostConstruct
+    public void loadData() {
+
+        students = new ArrayList<>();
 
         students.add(new Student("Max", "Verstappen"));
         students.add(new Student("Lando", "Norris"));
         students.add(new Student("Pierre", "Gasly"));
+
+    }
+
+    // define endpoint for "/students" - return a list of students
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
 
         return students;
     }
