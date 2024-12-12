5a2a2d7269345a049605d9dd64bdd1ef4acecd9e
Joshua Chuah <xjoshua96@gmail.com>
2024-12-12 18:31:45 -0500

    01-cruddemo-create

--- 03-springboot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/example/cruddemo/dao/StudentDAO.java
+++ 03-springboot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/example/cruddemo/dao/StudentDAO.java
@@ -9,15 +9,4 @@ public interface StudentDAO {
 
     void save(Student student);
 
-    Student findById(Integer id);
-
-    List<Student> findAll();
-
-    List<Student> findByLastName(String lastName);
-
-    void update(Student student);
-
-    void delete(Integer id);
-
-    int deleteAll();
 }
