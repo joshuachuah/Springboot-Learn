693c1a936cd46213880d47d308606136a0c74279
Joshua Chuah <xjoshua96@gmail.com>
2024-12-12 19:35:25 -0500

    04-path-variable-get-single-student

---
+++ 04-springboot-rest-crud/04-springboot-rest-crud-students-path-variable-get-single-student/src/main/java/com/example/demo/rest/StudentRestController.java
@@ -0,0 +1,44 @@
+package com.example.demo.rest;
+
+import com.example.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> students;
+
+    // define @PostConstruct to load the student data only once
+
+    @PostConstruct
+    public void loadData() {
+
+        students = new ArrayList<>();
+
+        students.add(new Student("Max", "Verstappen"));
+        students.add(new Student("Lando", "Norris"));
+        students.add(new Student("Pierre", "Gasly"));
+    }
+
+    // define endpoint for "/students" - return a list of students
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
+        return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return students.get(studentId);
+    }
+
+}
