f4790c9ba933d87d16e2ed3b2e112f281ed73091
Joshua Chuah <xjoshua96@gmail.com>
2024-12-16 18:51:50 -0500

    10-employee-service-find-add-delete-update

---
+++ 04-springboot-rest-crud/10-springboot-rest-crud-employee-service-find-add-delete-update/src/main/java/com/example/demo/dao/EmployeeDAOJpaImpl.java
@@ -0,0 +1,66 @@
+package com.example.demo.dao;
+
+import com.example.demo.entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOJpaImpl implements EmployeeDAO {
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // set up constructor injection
+    @Autowired
+    public EmployeeDAOJpaImpl(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+
+    @Override
+    public List<Employee> findAll(){
+
+        // create a query
+        TypedQuery<Employee> theQuery = entityManager.createQuery("FROM Employee", Employee.class);
+
+        // execute query and get result list
+        List<Employee> employees = theQuery.getResultList();
+
+        // return the results
+        return employees;
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        // get employee
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+
+        // return the employee
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+
+        // save employee (merge methods works by saving or updating depending on the id of the entity)
+        Employee dbEmployee = entityManager.merge(theEmployee);
+
+        // return dbEmployee
+        return dbEmployee;
+    }
+
+
+    @Override
+    public void deleteById(Employee theId) {
+
+        // find employee by id
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+
+        // remove employee
+        entityManager.remove(theEmployee);
+
+    }
+}
---
+++ 04-springboot-rest-crud/10-springboot-rest-crud-employee-service-find-add-delete-update/src/main/java/com/example/demo/service/EmployeeServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.EmployeeDAO;
+import com.example.demo.entity.Employee;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    public EmployeeDAO employeeDAO;
+
+    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
+        employeeDAO = theEmployeeDAO;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Override
+    @Transactional
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
+    }
+}
