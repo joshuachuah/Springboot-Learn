3aa5d9ced66806998a76090b14e691c75ddd933c
Joshua Chuah <xjoshua96@gmail.com>
2024-12-21 13:49:17 -0500

    12-form-data-binding-checkboxes-basic

---
+++ 06-springboot-spring-mvc/12-form-data-binding-checkboxes-basic/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${favoriteLanguage}")
+    private List<String> favoriteLanguage;
+
+    // method to show form
+    @GetMapping("/showStudentForm")
+    public String showForm(Model theModel){
+
+        // create a student object
+        Student theStudent = new Student();
+
+        // add student object to the model
+        theModel.addAttribute("student", theStudent);
+
+        // add list of countries
+        theModel.addAttribute("countries", countries);
+
+        // add favorite language
+        theModel.addAttribute("favoriteLanguage", favoriteLanguage);
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent){
+
+        // log input data
+
+        System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+
+        return "student-confirmation";
+    }
+}
