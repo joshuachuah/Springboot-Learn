1b106dbe735348e6117c7585d1dfaa476b1d6ac8
Joshua Chuah <xjoshua96@gmail.com>
2024-12-25 19:36:04 -0500

    02-find-instructor-by-id

---
+++ 09-springboot-jpa-advanced-mappings/02-jpa-one-to-one-uni-find-instructor/src/main/java/com/example/demo/dao/AppDAO.java
@@ -0,0 +1,10 @@
+package com.example.demo.dao;
+
+import com.example.demo.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findById(int theId);
+}
