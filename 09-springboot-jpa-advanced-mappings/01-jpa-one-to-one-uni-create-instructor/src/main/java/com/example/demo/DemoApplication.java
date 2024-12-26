931ef9e45c69d64a41061aa006f926f5551cac84
Joshua Chuah <xjoshua96@gmail.com>
2024-12-25 19:18:01 -0500

    01

--- 09-springboot-jpa-advanced-mappings/01-jpa-one-to-one-uni-create-instructor/src/main/java/com/example/demo/DemoApplication.java
+++ 09-springboot-jpa-advanced-mappings/01-jpa-one-to-one-uni-create-instructor/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import com.example.demo.dao.AppDAO;
+import com.example.demo.entity.Instructor;
+import com.example.demo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +16,35 @@ public class DemoApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 
         return runner ->
-                System.out.println("Hello World");
+                createInstructor(appDAO);
+    }
+
+    private void createInstructor(AppDAO appDAO) {
+
+        // create the instructor
+        Instructor tempInstructor =
+                new Instructor("Joshua", "Chuah", "joshuachuah@hello.com");
+
+        // create instructor detail
+        InstructorDetail tempInstructorDetail =
+                new InstructorDetail(
+                        "http://www.lesserafim.com/youtube",
+                        "Learning new technologies");
+
+        // associate the objects
+        tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+        // save the instructor
+        //
+        // NOTE: this will also save the details object
+        // because of CascadeType.ALL
+        //
+        System.out.println("Saving instructor:" + tempInstructor);
+        appDAO.save(tempInstructor);
+
+        System.out.println("DONE");
     }
 }
