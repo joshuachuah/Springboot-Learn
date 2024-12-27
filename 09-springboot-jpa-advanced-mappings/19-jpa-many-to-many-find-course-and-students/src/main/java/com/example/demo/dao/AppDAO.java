8134048f207740bdd1c427cb99f2bc2374282987
Joshua Chuah <xjoshua96@gmail.com>
2024-12-26 19:14:18 -0500

    19-find-course-and-students

---
+++ 09-springboot-jpa-advanced-mappings/19-jpa-many-to-many-find-course-and-students/src/main/java/com/example/demo/dao/AppDAO.java
@@ -0,0 +1,38 @@
+package com.example.demo.dao;
+
+import com.example.demo.entity.Course;
+import com.example.demo.entity.Instructor;
+import com.example.demo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+
+    List<Course> findCoursesByInstructorId(int theId);
+
+    Instructor findInstructorByIdJoinFetch(int theId);
+
+    void update(Instructor tempInstructor);
+
+    void update(Course tempCourse);
+
+    Course findCourseById(int theId);
+
+    void deleteCourseById(int theId);
+
+    void save(Course theCourse);
+
+    Course findCourseAndReviewsByCourseId(int theId);
+
+    Course findCourseAndStudentsByCourseId(int theId);
+}
