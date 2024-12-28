8fc2801361c3f3a8ea361a55478ee7775ee0b4d2
Joshua Chuah <xjoshua96@gmail.com>
2024-12-28 14:13:56 -0500

    09-match-any-method-in-package

---
+++ 10-springboot-aop/09-springboot-aop-pointcut-expressions-match-any-method-in-package/src/main/java/com/example/demo/dao/AccountDAO.java
@@ -0,0 +1,10 @@
+package com.example.demo.dao;
+
+import com.example.demo.Account;
+
+public interface AccountDAO {
+
+    void addAccount(Account theAccount, boolean vipFlag);
+
+    boolean doWork();
+}
