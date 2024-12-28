bf9a9f99e11ea9b09a1b053d461fb35e9ab365fb
Joshua Chuah <xjoshua96@gmail.com>
2024-12-28 13:58:36 -0500

    08-match-method-any-params

---
+++ 10-springboot-aop/08-springboot-aop-pointcut-expressions-match-method-any-params/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import com.example.demo.dao.AccountDAO;
+import com.example.demo.dao.MembershipDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
+
+		return runner -> {
+			demoBeforeAdvice(theAccountDAO, theMembershipDAO);
+		};
+	}
+
+	private void demoBeforeAdvice(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
+
+		// call the business method
+		Account myAccount = new Account();
+		theAccountDAO.addAccount(myAccount, true);
+
+		theMembershipDAO.addSillyMember();
+
+	}
+}
