56b5ebec6991296a25a368bb8967e7a40e4fcb3d
Joshua Chuah <xjoshua96@gmail.com>
2024-12-28 13:52:39 -0500

    07-match-method-with-account-and-more-params

---
+++ 10-springboot-aop/07-springboot-aop-pointcut-expressions-match-method-with-account-and-more-params/src/main/java/com/example/demo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,22 @@
+package com.example.demo.aspect;
+
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+
+    // add all advices for logging
+
+
+    // start with @Before Advice
+
+    // this is a pointcut expression
+    @Before("execution(* add*(com.example.demo.Account, ..))")
+    public void beforeAddAccountAdvice() {
+
+        System.out.println("\n=====>>> Executing @Before advice on method");
+    }
+}
