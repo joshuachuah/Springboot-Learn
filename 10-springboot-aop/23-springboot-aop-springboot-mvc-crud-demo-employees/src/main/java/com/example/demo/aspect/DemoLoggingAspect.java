5facc0e7fa4ff35ac6daec13b2f228daa924369d
Joshua Chuah <xjoshua96@gmail.com>
2024-12-29 19:03:32 -0500

    23-mvc-crud-demo-employees

--- 10-springboot-aop/23-springboot-aop-springboot-mvc-crud-demo-employees/src/main/java/com/example/demo/aspect/DemoLoggingAspect.java
+++ 10-springboot-aop/23-springboot-aop-springboot-mvc-crud-demo-employees/src/main/java/com/example/demo/aspect/DemoLoggingAspect.java
@@ -1,4 +1,71 @@
 package com.example.demo.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.hibernate.mapping.Join;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Aspect
+@Component
 public class DemoLoggingAspect {
+
+    // setup logger
+    private Logger myLogger = Logger.getLogger(getClass().getName());
+
+    // setup pointcut declarations
+    @Pointcut("execution(* com.example.demo.controller.*.*(..))")
+    private void forControllerPackage(){
+
+    }
+
+    @Pointcut("execution(* com.example.demo.service.*.*(..))")
+    private void forServicePackage(){
+
+    }
+
+    @Pointcut("execution(* com.example.demo.dao.*.*(..))")
+    private void forDaoPackage(){
+    }
+
+    @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+    private void forAppFlow(){
+
+    }
+
+    @Before("forAppFlow()")
+    public void before(JoinPoint theJoinPoint){
+
+        // display method we are calling
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        myLogger.info("=====>>> in @Before: calling method" + theMethod);
+
+        // display arguments to the method
+
+        // get the arguments
+        Object[] args = theJoinPoint.getArgs();
+
+        // loop through and display args
+        for (Object arg : args) {
+            myLogger.info("=====>>> argument: " + arg);
+        }
+    }
+
+    @AfterReturning(
+            pointcut = "forAppFlow()",
+            returning = "theResult"
+    )
+    public void afterReturning(JoinPoint theJoinPoint, Object theResult){
+
+        // display method we are returning from
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        myLogger.info("=====>>> in @AfterReturning: calling method" + theMethod);
+
+        // display data returned
+        myLogger.info("=====>>> returning: " + theResult);
+    }
 }
